4b9ab2d2c87e81ef796718bf5966a6da4055a773
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-03-25 18:12:09 +0700

    use mongol db and organizing code

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mysql from "mysql"
-
-const app = express();
-
-
+import mongoose from "mongoose"
 import { configViewEngine } from "./config/ViewEngine.js";
 import { initWebRoutes } from "./routes/WebRoutes.js";
 import dotenv from "dotenv";
 
+const app = express();
 
+/* CONFIGURATION SETUP */
 dotenv.config();
 
 app.use(cors());
@@ -18,31 +16,18 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "your_database_password",
-  database: "your_database_name"
-})
-
-db.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-  } else {
-    console.log('Connected to MySQL database!');
-  }
-});
-
-//config part
 configViewEngine(app);
 initWebRoutes(app);
 
-let port = process.env.PORT || 6969;
-// !Port => 6969
+/* MONGOOSE AND SERVER SETUP */
+let PORT = process.env.PORT || 6969;
 
-
-app.listen(port,() =>{
-    let notification = "\x1b[1m\x1b[90mWelcome to the internet!!! \x1b[37mPORT: \x1b[36m\x1b[4m" + port + '\x1b[0m'
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT,() =>{
+    let notification = "\x1b[1m\x1b[90mWelcome to the internet!!! \x1b[37mPORT: \x1b[36m\x1b[4m" + PORT + '\x1b[0m'
     let _6969 =        "\x1b[1m\x1b[90mIf the U see \x1b[37mPORT: \x1b[36m\x1b[4m6969\x1b[0m\x1b[1m\x1b[90m! then you FUCKED UP \x1b[33mヽ(*・ω・)ﾉ\x1b[0m"
 
     console.log(      "\x1b[35m===============================================================================================\x1b[0m")
@@ -52,5 +37,13 @@ app.listen(port,() =>{
     console.log("\x1b[35m|\x1b[0m                     " +         _6969                                   +"                 \x1b[35m|\x1b[0m")
     console.log("\x1b[35m|\x1b[0m                                                                                             \x1b[35m|\x1b[0m")
     console.log(     "\x1b[35m===============================================================================================\x1b[0m")
-})
+  })
+}).catch((error) => console.log(error))
+
+
+
+
+
+
+
 
