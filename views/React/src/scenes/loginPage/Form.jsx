8a3ef5ccf84fb6f8f0a4d0bf13ea2fd4cba9c42e
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-03-26 06:00:28 +0700

    Update form scenes in log in/ registration page

--- views/React/src/scenes/loginPage/Form.jsx
+++ views/React/src/scenes/loginPage/Form.jsx
@@ -59,52 +59,50 @@ const Form  = () => {
     const dark = theme.palette.neutral.dark;
 
     const register = async (values, onSubmitProps) => {
-        // This allows for sending form info with image
-        const formData = new formData();
-        for (let value in values) {
-            formData.append(value, values[value])
+      // this allows us to send form info with image
+      const formData = new FormData();
+      for (let value in values) {
+        formData.append(value, values[value]);
+      }
+      formData.append("picturePath", values.picture.name);
+  
+      const savedUserResponse = await fetch(
+        "http://localhost:5000/auth/register",
+        {
+          method: "POST",
+          body: formData,
         }
-        formData.append('picturePath', values.picture.name);
-
-        const savedUserResponse = await fetch(
-            "http://localhost:5000/auth/register",
-            {
-                method: POST,
-                body: formData
-            }
-        );
-        const savedUser = await savedUserResponse.json();
-        onSubmitProps.resestForm();
-
-        if (savedUser) {
-            setPageType("login");
-        }
+      );
+      const savedUser = await savedUserResponse.json();
+      onSubmitProps.resetForm();
+  
+      if (savedUser) {
+        setPageType("login");
+      }
     };
-
+  
     const login = async (values, onSubmitProps) => {
-        const loggedInUserResponse = await fetch(
-            "http://localhost:5000/auth/login",
-            {
-                method: POST,
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(values),
-            }
+      const loggedInResponse = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
+      const loggedIn = await loggedInResponse.json();
+      onSubmitProps.resetForm();
+      if (loggedIn) {
+        dispatch(
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token,
+          })
         );
-        const loggedIn = await loggedInUserResponse.json();
-        onSubmitProps.resetForm();
-        if (loggedIn) {
-            dispatch(
-                setLogin({
-                    user: loggedIn.user,
-                    token: loggedIn.token,
-                })
-            );
-            navigate("/home");
-        }
-    }
+        navigate("/home");
+      }
+    };
+  
     const handleFormSubmit = async (values, onSubmitProps) => {
-        if (isLogin) await login(values, onSubmitProps);
-        if (isRegister) await registerSchema(values, onSubmitProps);
+      if (isLogin) await login(values, onSubmitProps);
+      if (isRegister) await register(values, onSubmitProps);
     };
 
     return (
@@ -176,7 +174,7 @@ const Form  = () => {
                         p="1rem"
                         sx={{ "&:hover": { cursor: "pointer" } }}
                       >
-                        <input {...getInputProps()} />
+                        <input {...getInputProps()} name="picture"/>
                         {!values.picture ? (
                           <p>Add Picture Here</p>
                         ) : (
