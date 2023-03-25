4270851a4186894e5dd36ab0f72fea205458dc7c
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-03-25 14:41:21 +0700

    re-structure controller
    Re-structer the controller as the idea of Khang and adding tutorial for new structure

--- models/sample.js
+++ models/sample.js
@@ -1,13 +1,26 @@
-import express from "express";
-var router = express.Router();
+// write some function to solve logic problem here and export it main function
 
-/* rest API:
-                method get khi lấy data
-                method Post khi create
-                method Put để cập nhật
-                method delete để xóa
-    */
+//example:
+var DoSth_1 = (a,b) => {
+    a +=b
+    return a
+}
+var DoAnotherThing = (a,b) => {
+    a -= b
+    return a
+}
+var main = (req) => {
+    if (!req){
+        let a = 1
+        let b = 2
+        let c = DoSth_1(a,b)
+        c = DoAnotherThing(c,b)
+        return c
+    }
+    else{
+        console.log("\x1b[37m\x1b[36mMODELS CALLED!!!\x1b[0m")
+    }
+}
 
 
-
-module.exports = router
+export default {main}
