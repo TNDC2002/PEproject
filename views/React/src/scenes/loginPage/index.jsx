0e5d2dc6be5c132c047a389c7970eac06082fcc1
bob3214y3 <18230@student.vgu.edu.vn>
2023-04-03 00:40:16 +0700

    The Login Page and Registration Page Design
    the brand new design of registration page and login page merge in with the same functionality

--- views/React/src/scenes/loginPage/index.jsx
+++ views/React/src/scenes/loginPage/index.jsx
@@ -5,44 +5,14 @@ import {
     useMediaQuery
 } from "@mui/material";
 import Form from "../loginPage/Form"
-
+import NewDesign from "../loginPage/NewDesign";
 const LoginPage = () => {
-    const theme = useTheme();
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
-    return <Box>
-        <Box 
-            width="100%" 
-            backgroundColor={theme.palette.background.alt} 
-            p="1rem 6%" 
-            textAlign="center"
-        >
-            <Typography 
-                    fontWeight="bold"
-                    fontSize="32px"
-                    color="primary"
-                    >
-                    VNGU Films
-            </Typography>
-        </Box>
 
-        <Box
-            width= {isNonMobileScreens ? "50%" : "93%"}
-            p="2rem"
-            m="2rem auto"
-            borderRadius="1.5rem"
-            backgroundColor={theme.palette.background.alt}
-        >
-            <Typography
-                fontWeight="500"
-                variant="h5"
-                sx ={{
-                    mb: "1.5rem"
-                }}
-            >
-                Welcome to VNGU Films, the best platform provider for your favourite movies and TV Shows!
-            </Typography>
-        </Box>
-        <Form />
+    return <Box>
+        <NewDesign></NewDesign>
+        
+        
+       
     </Box>;
 };
 
