de1dc22b30db576ff6c82252df4f2e7c333115b6
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-04-05 15:54:37 +0700

    Add route for /list

--- server/routes/movieAPI.js
+++ server/routes/movieAPI.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { favourite, checkFavourite } from "../controller/movieAPI.js";
+import { favourite, checkFavourite, getList } from "../controller/movieAPI.js";
 
 const router = express.Router();
 
 router.post("/favourite", favourite);
 router.post("/favourite/check", checkFavourite);
+router.get("/list", getList);
+
 export default router;
