a1de055421c067141339b48c329e8c536aec8a70
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-04-06 14:38:29 +0700

    Add getTrailerID controller

--- server/routes/movieAPI.js
+++ server/routes/movieAPI.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { favourite, checkFavourite, getDetail, getTrailer } from "../controller/movieAPI.js";
+import { favourite, checkFavourite, getDetail, getTrailerID } from "../controller/movieAPI.js";
 
 const router = express.Router();
 
 router.post("/favourite", favourite);
 router.post("/favourite/check", checkFavourite);
 router.get("/detail/:movieID", getDetail);
-router.get("/trailer/:movieID", getTrailer);
+router.get("/trailer/:movieID", getTrailerID);
 export default router;
