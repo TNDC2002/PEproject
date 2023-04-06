3de4c6c38b5e68476156d4655603b863cea3f0ed
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-04-06 14:37:23 +0700

    Add get trailer route

--- server/routes/movieAPI.js
+++ server/routes/movieAPI.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { favourite, checkFavourite, getDetail } from "../controller/movieAPI.js";
+import { favourite, checkFavourite, getDetail, getTrailer } from "../controller/movieAPI.js";
 
 const router = express.Router();
 
 router.post("/favourite", favourite);
 router.post("/favourite/check", checkFavourite);
 router.get("/detail/:movieID", getDetail);
+router.get("/trailer/:movieID", getTrailer);
 export default router;
