3c221ea7c0e6a2a42f9e8614ffc48b7a8ee9b73d
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-04-24 13:45:24 +0700

    Complete autocomple search bar

--- views/React/src/scenes/navbar/SearchBar.jsx
+++ views/React/src/scenes/navbar/SearchBar.jsx
@@ -5,37 +5,75 @@ import Autocomplete from '@mui/material/Autocomplete';
 import { useTheme, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-const defaultSearchOptions = 
-            [{label: "original value 1"},
-            {label: "original value 2"}];
-
 const SearchBar = () => {
   const [isFocused, setIsFocused] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [defaultSearchOptions, setDefaultSearchOptions] = useState([]);
   const [isEmpty, setIsEmpty] = useState(true);
-  const [options, setOptions] = useState(defaultSearchOptions);
+  const [options, setOptions] = useState([]);
   const theme = useTheme();
-
   const user = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
 
   const navigate = useNavigate();
 
-  const uploadUserSearch = async (userID, searchedString) => {
+
+  useEffect(() => {
+    const fetchUserSearch = async (userID, numberOfEntry) => {
+      try {
+        const fetchUserSearchResponse = await fetch(
+          `http://localhost:5000/user-search-history?userID=${userID}&limit=${numberOfEntry}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json"
+            }
+          } 
+        );
+        if (!fetchUserSearchResponse.ok) {
+          throw new Error('Failed to fetch user search data');
+        }
+        const userSearchData = await fetchUserSearchResponse.json();
+        const searchedStrings = userSearchData.map((search) => search.searchedString);
+  
+        setDefaultSearchOptions(await searchedStrings.map((search) => {
+          return {
+            label: search,
+          };
+        }));
+      } catch (error) {
+        console.error(error);
+      };
+    };
+    fetchUserSearch(user._id, 5);
+  }, []);
+
+  useEffect(() => {
+    setOptions(defaultSearchOptions);
+  }, [defaultSearchOptions]);
+
+  useEffect(() => {
+    if (options.at(0) === '') {
+      options.push();
+    }
+  }, [options])
+
+  const insertUserSearch = async (userID, searchedString) => {
     const requestData = {
       userID: userID,
       searchedString: searchedString,
       createdAt: new Date()
     };
 
-    const uploadUserSearchResponse = await fetch(
-      "http://localhost:5000/user/search/insert",
+    const insertUserSearchResponse = await fetch(
+      "http://localhost:5000/user-search-history/insert",
       {
         method: "POST",
         headers: { 
           Authorization: `Bearer ${token}`,
           "Content-Type": "application/json"
-          
         },
         body: JSON.stringify(requestData),
       }
@@ -51,29 +89,42 @@ const SearchBar = () => {
   };
 
   const handleSearch = async (value) => {
-    uploadUserSearch(user._id, inputValue);
-    
+    if (!selectedOption && value !== "") {
+      insertUserSearch(user._id, value);
+      navigate(`/home/search/?query=${value}`);
+    }
   };
   
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (selectedOption) {
+        setSelectedOption(false);
+      }
+      handleSearch(inputValue);
+      
+    }
+  };
   
 
   const handleInputChange = async (event, newInputValue) => {
     setInputValue(newInputValue);
     const alreadyExists = options.some((option) => option.label === newInputValue);
-    if (!alreadyExists && newInputValue !== "") {
+    if (!alreadyExists) {
       setOptions([{ label: newInputValue }, ...defaultSearchOptions]);
     }
   };
 
   return (
     <Autocomplete
-      freeSolo
       options={options}
       getOptionLabel={(option) => option.label || ""}
-      onChange={(value) => handleSearch(value)}
       inputValue={inputValue}
       sx={{ width: '300px' }}
+      onHighlightChange={(event, option) =>
+        option === null ? setSelectedOption(false) : setSelectedOption(true)
+      }
       onInputChange={handleInputChange}
+      onKeyDown={handleKeyDown}
       renderInput={(params) => (
         <TextField
           {...params}
