2356485ce8c4eb40b562719f6f3e1f1dd260e246
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-05-07 21:18:24 +0700

    add a global movie id variable

--- views/React/src/scenes/homePage/MovieCard.jsx
+++ views/React/src/scenes/homePage/MovieCard.jsx
@@ -9,32 +9,44 @@ import Image from 'mui-image'
 import React, { useState } from "react";
 
 const MovieCard = ({ movie }) => {
-    const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    const navigate = useNavigate();
-    const theme = useTheme();
-    const neutralLight = theme.palette.neutral.light;
+  const [hoveredMovieId, setHoveredMovieId] = useState(null);
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
 
-    return (
-      <Box 
-      backgroundColor={neutralLight}
-      onClick={() => navigate(`/movie/${movie.id}`)}
-      sx={{
-        "&:hover":{
-        cursor: "pointer"
-        }
-      }}
-      >
-        <Image 
-          src={imageUrl}
-          width={200 / 1}
-          height={300 / 1}
-          alt={`${movie.title} poster`}
-        />
-      </Box>
-    );
+  const handleMouseEnter = () => {
+    const hoveredMovieId = movie.id;
+    setHoveredMovieId(hoveredMovieId);
+    window.hoveredMovieId = hoveredMovieId;
+    console.log(`Hovering at ${hoveredMovieId}`);
   };
   
+  const handleMouseLeave = () => {
+    setHoveredMovieId(null);
+    window.hoveredMovieId = null;
+  };
 
+  return (
+    <Box 
+      backgroundColor={neutralLight}
+      onClick={() => navigate(`/movie/${movie.id}`)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        "&:hover":{
+          cursor: "pointer"
+        }
+      }}
+    >
+      <Image 
+        src={imageUrl}
+        width={200 / 1.5}
+        height={300 / 1.5}
+        alt={`${movie.title} poster`}
+      />
+    </Box>
+  );
+};
 
-  export default MovieCard;
-  
+export default MovieCard;
