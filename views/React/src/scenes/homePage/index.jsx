c6e195aea0205ffbed522c74769a6663038785e1
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-05-07 09:41:10 +0700

    format

--- views/React/src/scenes/homePage/index.jsx
+++ views/React/src/scenes/homePage/index.jsx
@@ -5,18 +5,17 @@ import Navbar from "../navbar";
 import MovieList from "./MovieList";
 import YouTubePlayer from "../trailerPlayer/YoutubeVideo";
 
-
 const HomePage = () => {
-    return (
-        <div>
-            <Navbar/>
-            <YouTubePlayer videoId={""}/>
-            <MovieList category="popular" />
-            <MovieList category="nowPlaying" />
-            <MovieList category="topRated" />
-            <MovieList category="upcoming" />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <YouTubePlayer videoId={""} />
+      <MovieList category="popular" />
+      <MovieList category="nowPlaying" />
+      <MovieList category="topRated" />
+      <MovieList category="upcoming" />
+    </div>
+  );
 };
 
 export default HomePage;
