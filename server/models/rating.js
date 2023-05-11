7844a71d9fb07d2c78e70d65dc3733b4ae332082
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 12:29:48 +0700

    create Delete function

--- server/models/rating.js
+++ server/models/rating.js
@@ -49,7 +49,29 @@ const PUT_rating = async (req, res) => {
 
     }
 }
+const DELETE_rating = async (req, res) => {
+    try {
+        const { userID, movieID, rating } = req.body
+        let Rated = await UserRateMovie.findOne({
+            userID: userID,
+            movieID:movieID,
+            rating: rating
+        });
+        Rated.save()
+            .then(() => {
+            })
+            .catch((error) => {
+                console.log("ERROR --- Rating.js --- can't UPDATE to DB")
+            })
 
+    } catch (err) {
+        return {
+            status: 500,
+            error: err.message
+        }
+
+    }
+}
 var output = {
     POST: POST_rating,
     PUT: PUT_rating
