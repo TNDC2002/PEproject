f3532c4a4bd437145e5de13088fb691c7eca5235
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 11:55:18 +0700

    save rating to db

--- server/models/rating.js
+++ server/models/rating.js
@@ -6,7 +6,30 @@ const rating = async (req, res) => {
     try {
         const {userID, movieID, rating} = req.body
         
-
+        const newVerification = new UserRateMovie({
+            userID: userID,
+            movieID: movieID,
+            rating: rating,
+        });
+        newVerification.save()
+            .then(() => {
+                transporter.sendMail(mailOption)
+                    .then(() => {
+                        res.json({
+                            status: "PENDING",
+                            message: "Verification email sent!"
+                        });
+                    })
+                    .catch((error) => {
+                    })
+            })
+            .catch((error) => {
+                console.log(error);
+                res.json({
+                    status: "FAILED",
+                    message: "Couldn't save verification email data..."
+                });
+            })
     
     } catch (err) {
         res.status(500).json({ error: err.message });
