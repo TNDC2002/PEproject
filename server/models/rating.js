561136c0caa59f73171a25813278c0bf5597fa51
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 11:39:13 +0700

    create rating model

--- server/models/rating.js
+++ server/models/rating.js
@@ -1,21 +1,13 @@
 import jwt from "jsonwebtoken";
 
-export const verifyToken = async (req, res, next) => {
+const verifyToken = async (req, res, next) => {
     try {
         let token = req.header("Authorization");
-
-        if(!token) {
-            return res.status(403).send("Access Denied");
-        }
         
         if(token.startsWith("Bearer ")) {
             token = token.slice(7, token.length).trimLeft();
-            console.log("TOKEN: ",token)
         }
-
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = verified;
-        next();
+        console.log(token)
     } catch(err) {
         res.status(500).json({ error: err.message });
     }
