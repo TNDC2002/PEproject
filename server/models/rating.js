9860ea5b539fbf8da7c0ac0ac07bd266ac22e7ae
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 11:41:47 +0700

    catch user ID

--- server/models/rating.js
+++ server/models/rating.js
@@ -1,19 +1,56 @@
-import jwt from "jsonwebtoken";
+import UserSearchHistory from "../models/UserSearchHistory.js"
 
-const verifyToken = async (req, res, next) => {
+
+/* INSERT USER SEARCH STRING */
+export const insertSearch = async (req, res) => {
     try {
-        let token = req.header("Authorization");
-        
-        if(token.startsWith("Bearer ")) {
-            token = token.slice(7, token.length).trimLeft();
+        const {userID, searchedString, createdAt} = req.body
+
+        // Check if a search entry with the same userID and searchedString already exists
+        const existingSearchEntry = await UserSearchHistory.findOneAndUpdate(
+            { userID, searchedString },
+            { createdAt },
+            { new: true }
+        );
+
+        if (existingSearchEntry) {
+            res.status(200).json(existingSearchEntry);
+        } else {
+            const newSearchEntry = new UserSearchHistory({
+                userID,
+                searchedString,
+                createdAt
+            });
+
+            const savedSearchEntry = await newSearchEntry.save();
+            res.status(201).json(savedSearchEntry);
         }
-        console.log(token)
-    } catch(err) {
+    
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
 }
-var output = {
-    verifyToken
+
+
+/* FETCH USER SEARCH DATA */
+export const fetchSearches = async (req, res) => {
+    try {
+        const userID = req.query.userID;
+        const limit = parseInt(req.query.limit) || 10;
+      
+        const searchHistory = await UserSearchHistory.find({ userID })
+          .limit(limit)
+          .sort({ updatedAt: -1});
+      
+        res.json(searchHistory);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
 
+var output = {
+    insertSearch,
+    fetchSearches
+};
 export default output;
