2888ea84518ae707d1fc6b059c0f788e5cd074f1
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 15:14:16 +0700

    create GET processor

--- server/models/rating.js
+++ server/models/rating.js
@@ -7,12 +7,28 @@ const calc = (movieID, rating, weight) => {
             movieID: movieID,
             rating: rating
         });
+        let rate = (Rated.rating * Rated.weight + rating * weight)/(Rated.weight + weight)
+
         Rated.save()
             .then(() => {
             })
             .catch((error) => {
                 console.log("ERROR --- Rating.js --- can't UPDATE to DB")
             })
+
+    } catch (err) {
+        return {
+            status: 500,
+            error: err.message
+        }
+
+    }
+}
+
+const GET_rating = async (req) => {
+    try {
+        const { userID, movieID } = req.body
+
 
     } catch (err) {
         return {
@@ -51,21 +67,7 @@ const POST_rating = async (req) => {
 const PUT_rating = async (req) => {
     try {
         const { userID, movieID, rating } = req.body
-        let Rated = await UserRateMovie.findOne({
-            userID: userID,
-            movieID: movieID
-        });
-        Rated = {
-            userID: userID,
-            movieID: movieID,
-            rating: rating
-        }
-        Rated.save()
-            .then(() => {
-            })
-            .catch((error) => {
-                console.log("ERROR --- Rating.js --- can't UPDATE to DB")
-            })
+        
 
     } catch (err) {
         return {
