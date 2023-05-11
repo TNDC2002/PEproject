24731fa521e7c56f1dc894436af59726f6f36c66
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-11 11:48:20 +0700

    delete useless lines

--- server/models/rating.js
+++ server/models/rating.js
@@ -6,25 +6,6 @@ const rating = async (req, res) => {
     try {
         const {userID, searchedString, createdAt} = req.body
 
-        // Check if a search entry with the same userID and searchedString already exists
-        const existingSearchEntry = await UserSearchHistory.findOneAndUpdate(
-            { userID, searchedString },
-            { createdAt },
-            { new: true }
-        );
-
-        if (existingSearchEntry) {
-            res.status(200).json(existingSearchEntry);
-        } else {
-            const newSearchEntry = new UserSearchHistory({
-                userID,
-                searchedString,
-                createdAt
-            });
-
-            const savedSearchEntry = await newSearchEntry.save();
-            res.status(201).json(savedSearchEntry);
-        }
     
     } catch (err) {
         res.status(500).json({ error: err.message });
