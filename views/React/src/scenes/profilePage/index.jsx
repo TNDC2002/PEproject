a31c3d27e1790596a744ea144aeeac26e14e2df4
bob3214y3 <18230@student.vgu.edu.vn>
2023-05-14 05:44:56 +0700

    add animation

--- views/React/src/scenes/profilePage/index.jsx
+++ views/React/src/scenes/profilePage/index.jsx
@@ -2,7 +2,8 @@ import profileCard from "../../assets/image/profileBox7.png";
 import accountCard from "../../assets/image/accountBox.png";
 import movieCard from "../../assets/image/movieBox.png";
 import Background from "../../assets/image/backgroundNavPage3.png";
-import { Link, Route, Router } from 'react-router-dom';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Box,
   Button,
@@ -13,8 +14,51 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import Original from "./Original";
+import { styled } from "@mui/material/styles";
+
+const StyledBox = styled(Box)({
+  position: 'relative',
+  display: 'flex',
+  
+  boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  width: 250,
+  height: 250,
+  transition: "all 0.3s ease-in-out",
+  cursor: 'pointer',
+  "&:hover": {
+    top: '100%',
+    opacity: 1,
+    transition: 'all 0.5s ease-in-out',
+    transform: "scale(1.2)",
+    zIndex: 1,
+  },
+});
+
+const DescriptionPanel = styled(Box)({
+  position: "absolute",
+  top: "100%",
+  backgroundColor: "#fff",
+  padding: "10px",
+  borderRadius: "5px",
+  boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
+  width: "250px",
+  marginTop: "10px",
+  transition: 'all 0.5s ease-in-out'
+  
+});
 const ProfilePage = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
     <Box
       display="flex"
@@ -27,6 +71,8 @@ const ProfilePage = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <Typography
         style={{ color: "#B3005E", marginTop: "50px", marginBottom: "50px" }}
@@ -35,14 +81,18 @@ const ProfilePage = () => {
         Yo bruh, what's the move?
       </Typography>
 
-      <Stack direction="row" spacing={15} style={{ marginTop: "60px", opacity: 1, }}>
+      <Stack
+        direction="row"
+        spacing={15}
+        style={{ marginTop: "60px", opacity: 1 }}
+      >
         
-      
-        <Box
-        
+        <StyledBox
           display="flex"
           justifyContent="center"
           alignItems="center"
+          onMouseEnter={() => setIsHovering(true)}
+          onMouseLeave={() => setIsHovering(false)}
           sx={{
             backgroundImage: `url(${accountCard})`,
 
@@ -52,11 +102,17 @@ const ProfilePage = () => {
 
             width: 250,
             height: 250,
+            
           }}
           
-          
         >
-          
+          {isHovering && (
+        <DescriptionPanel>
+          <Typography fontWeight="light" style={{ color: "#B3005E" }} fontSize={18}>
+            This is a description panel for the Account box.
+          </Typography>
+        </DescriptionPanel>
+      )}
           <Typography
             fontWeight="light"
             style={{ color: "#B3005E", marginTop: "50px" }}
@@ -64,35 +120,39 @@ const ProfilePage = () => {
           >
             Account
           </Typography>
-          
-        </Box>
-        
-        <Link to="./Original">
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          sx={{
-            backgroundImage: `url(${profileCard})`,
+         
+        </StyledBox>
 
-            boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+        <Link to="./Original" style={{ color: "transparent" }}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              backgroundImage: `url(${profileCard})`,
 
-            width: 250,
-            height: 250,
-          }}
-        >
-          <Typography
-            fontWeight="light"
-            style={{ color: "#B3005E", marginTop: "50px" }}
-            fontSize={25}
+              boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+
+              width: 250,
+              height: 250,
+            }}
           >
-            Profile
-          </Typography>
-        </Box>
+            
+
+
+            
+            <Typography
+              fontWeight="light"
+              style={{ color: "#B3005E", marginTop: "50px" }}
+              fontSize={25}
+            >
+              Profile
+            </Typography>
+          </Box>
         </Link>
-        
+
         <Box
           display="flex"
           justifyContent="center"
