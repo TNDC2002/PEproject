a1c732af86c13aa1008facc97fa6731c3677cee9
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-13 12:54:21 +0700

    rename form

--- server/controller/RentalController.js
+++ server/controller/RentalController.js
@@ -5,7 +5,7 @@
 
 import * as Rating from "../models/Rating.js";
 
-const Sample_handler_GET = async (req, res) => {
+const Rental_GET = async (req, res) => {
     // GET the rating
     let Rating_return = await Rating.default.GET(req)
     if (Rating_return.status) {
@@ -15,7 +15,7 @@ const Sample_handler_GET = async (req, res) => {
     }
 }
 
-const Sample_handler_POST = async (req, res) => {
+const Rental_POST = async (req, res) => {
 
     // post the rating
     let Rating_return = await Rating.default.POST(req)
@@ -27,7 +27,7 @@ const Sample_handler_POST = async (req, res) => {
 
 }
 
-const Sample_handler_PUT = async (req, res) => {
+const Rental_PUT = async (req, res) => {
 
     //UPDATE the rate
     let Rating_return = await Rating.default.PUT(req)
@@ -39,7 +39,7 @@ const Sample_handler_PUT = async (req, res) => {
 
 }
 
-const Sample_handler_DELETE = async (req, res) => {
+const Rental_DELETE = async (req, res) => {
 
     //DELETE the rate
     let Delete_return = await Rating.default.DELETE(req)
@@ -52,9 +52,9 @@ const Sample_handler_DELETE = async (req, res) => {
 }
 
 const handler = {
-    GET_handler: Sample_handler_GET,
-    POST_handler: Sample_handler_POST,
-    PUT_handler: Sample_handler_PUT,
-    DELETE_handler: Sample_handler_DELETE
+    GET_Rental: Rental_GET,
+    POST_Rental: Rental_POST,
+    PUT_Rental: Rental_PUT,
+    DELETE_Rental: Rental_DELETE
 }
 export default handler
