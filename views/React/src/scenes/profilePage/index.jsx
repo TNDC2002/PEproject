62cabe346da47b42dc2e80434e797c7f4cca0aac
bob3214y3 <18230@student.vgu.edu.vn>
2023-05-14 10:02:14 +0700

    add animation

--- views/React/src/scenes/profilePage/index.jsx
+++ views/React/src/scenes/profilePage/index.jsx
@@ -45,19 +45,43 @@ const DescriptionPanel = styled(Box)({
   boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2)",
   width: "250px",
   marginTop: "10px",
+  
   transition: 'all 0.5s ease-in-out'
   
 });
 const ProfilePage = () => {
-  const [isHovering, setIsHovering] = useState(false);
+  const [isHoveringAccount, setIsHoveringAccount] = useState(false);
+  const [isHoveringProfile, setIsHoveringProfile] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovering(true);
+  const handleMouseEnterProfile = () => {
+    setIsHoveringProfile(true);
   };
 
-  const handleMouseLeave = () => {
-    setIsHovering(false);
+  const handleMouseLeaveProfile = () => {
+    setIsHoveringProfile(false);
   };
+  const [isHoveringMovie, setIsHoveringMovie] = useState(false);
+
+  const handleMouseEnterAccount = () => {
+    setIsHoveringAccount(true);
+  };
+
+  const handleMouseLeaveAccount = () => {
+    setIsHoveringAccount(false);
+  };
+
+  
+  
+
+  const handleMouseEnterMovie = () => {
+    setIsHoveringMovie(true);
+  };
+
+  const handleMouseLeaveMovie = () => {
+    setIsHoveringMovie(false);
+  };
+
+
 
   return (
     <Box
@@ -71,8 +95,7 @@ const ProfilePage = () => {
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+    
     >
       <Typography
         style={{ color: "#B3005E", marginTop: "50px", marginBottom: "50px" }}
@@ -91,8 +114,8 @@ const ProfilePage = () => {
           display="flex"
           justifyContent="center"
           alignItems="center"
-          onMouseEnter={() => setIsHovering(true)}
-          onMouseLeave={() => setIsHovering(false)}
+          onMouseEnter={handleMouseEnterAccount}
+          onMouseLeave={handleMouseLeaveAccount}
           sx={{
             backgroundImage: `url(${accountCard})`,
 
@@ -106,7 +129,7 @@ const ProfilePage = () => {
           }}
           
         >
-          {isHovering && (
+          {isHoveringAccount && (
         <DescriptionPanel>
           <Typography fontWeight="light" style={{ color: "#B3005E" }} fontSize={18}>
             This is a description panel for the Account box.
@@ -123,47 +146,21 @@ const ProfilePage = () => {
          
         </StyledBox>
 
+        
+
         <Link to="./Original" style={{ color: "transparent" }}>
-          <Box
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              backgroundImage: `url(${profileCard})`,
-
-              boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-
-              width: 250,
-              height: 250,
-            }}
-          >
-            
-
-
-            
-            <Typography
-              fontWeight="light"
-              style={{ color: "#B3005E", marginTop: "50px" }}
-              fontSize={25}
-            >
-              Profile
-            </Typography>
-          </Box>
-        </Link>
-
-        <Box
+      <div style={{ position: "relative" }}>
+        <StyledBox
           display="flex"
           justifyContent="center"
           alignItems="center"
+          onMouseEnter={handleMouseEnterProfile}
+          onMouseLeave={handleMouseLeaveProfile}
           sx={{
-            backgroundImage: `url(${movieCard})`,
-
+            backgroundImage: `url(${profileCard})`,
             boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
             backgroundSize: "cover",
             backgroundPosition: "center",
-
             width: 250,
             height: 250,
           }}
@@ -175,7 +172,68 @@ const ProfilePage = () => {
           >
             Movie
           </Typography>
-        </Box>
+        </StyledBox>
+        {isHoveringProfile && (
+          <DescriptionPanel
+            style={{
+              position: "absolute",
+              top: "100%",
+              left: 0,
+              opacity: 1,
+              transition: "opacity 0.3s ease-in-out",
+              zIndex: 1, // Add zIndex value here
+              
+            }}
+          >
+            
+            <Typography
+              fontWeight="light"
+              style={{ color: "#B3005E" }}
+              fontSize={18}
+            >
+              This is a description panel for the Profile box.
+            </Typography>
+          </DescriptionPanel>
+        )}
+      </div>
+    </Link>
+  
+
+        <StyledBox
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          onMouseEnter={handleMouseEnterMovie}
+          onMouseLeave={handleMouseLeaveMovie}
+          sx={{
+            backgroundImage: `url(${movieCard})`,
+
+            boxShadow: "0 8px 16px 0 rgba(0,0,0,0.4)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+
+            width: 250,
+            height: 250,
+            
+          }}
+          
+        >
+          {isHoveringMovie && (
+        <DescriptionPanel>
+          <Typography fontWeight="light" style={{ color: "#B3005E" }} fontSize={18}>
+            This is a description panel for the Movie box.
+          </Typography>
+        </DescriptionPanel>
+      )}
+          <Typography
+            fontWeight="light"
+            style={{ color: "#B3005E", marginTop: "50px" }}
+            fontSize={25}
+          >
+            Movie
+          </Typography>
+         
+        </StyledBox>
       </Stack>
     </Box>
   );
