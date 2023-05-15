01ae06b71d9c633a5f051f295d1338229832f53d
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-05-15 20:45:29 +0700

    Update user.js temporarily

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,52 @@
-import UserSearchHistory from "../models/UserSearchHistory.js"
-import UserFavouriteMovie from "../models/UserFavouriteMovie.js";
-import UserMovieRental from "../models/UserRentMovie.js";
+import UserSearchHistory from "../models/UserSearchHistory_Schema.js"
+import UserFavouriteMovie from "../models/UserFavouriteMovie_Schema.js";
+import UserMovieRental from "../models/UserRentMovie_Schema.js";
 import axios from "axios";
+import User from "../models/User_Schema.js";
+
+/* READ */
+export const getUser = async (req, res) => {
+    try {
+        const { userID } = req.params;
+        const user = await User.findOne({
+            _id: userID,
+        });
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+}
+
+export const updateUserProfile = async (req, res) => {
+    const { userID } = req.params;
+    const user = await User.findOne({
+        _id: userID,
+    });
+
+    console.log(req.body);
+
+    if (user) {
+        user.firstName = req.body.firstName || user.firstName;
+        user.lastName = req.body.lastName || user.lastName;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email,
+        })
+    }
+    else {
+        res.status(404).json({ message: err.message });
+    }
+}
 
 /* INSERT USER SEARCH STRING */
 export const insertSearch = async (req, res) => {
@@ -87,6 +132,8 @@ export const fetchFavourites = async (req, res) => {
     }
 }
 var output = {
+    getUser,
+    updateUserProfile,
     insertSearch,
     fetchSearches,
     fetchFavourites
