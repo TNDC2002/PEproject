01eefc7c836715be3ad7500b808473d58d8138a1
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-05-15 14:08:21 +0700

    Update mylist

--- views/React/src/scenes/mylistPage/index.jsx
+++ views/React/src/scenes/mylistPage/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Box } from "@mui/material";
+import Image from "mui-image";
 
 const MyListPage = () => {
     const [favouriteMovie, setFavouriteMovie ] = useState([]);
@@ -30,7 +32,26 @@ const MyListPage = () => {
     }, [])
     return (
         <div>
-            HELLO
+         <Box>
+            <strong>Your favourite movies:</strong>
+            {favouriteMovie.map((movie) => (
+            <div key={movie.id}>
+                <span>{movie.title}</span>
+                {/* Render additional movie details if needed */}
+                <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+            </div>
+            ))}
+        </Box>
+        <Box>
+            <strong>Your favourite shows:</strong>
+            {favouriteShow.map((show) => (
+            <div key={show.id}>
+                <span>{show.name}</span>
+                {/* Render additional show details if needed */}
+                <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${show.poster_path}`}/>
+            </div>
+            ))}
+        </Box>
         </div>
     )
 }
