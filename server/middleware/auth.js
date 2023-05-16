919e47bb0ad84ba388819fbab42a7a8da6bb5790
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-16 14:05:17 +0700

    improve middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,26 @@
 import jwt from "jsonwebtoken";
-import { sign, unsign } from 'cookie-signature';
+import User from "../models/User.js"
 export const verifyToken = async (req, res, next) => {
     try {
         console.log("____________verify___________")
         let token = req.signedCookies.token;
-        // token = unsign(token, process.env.Cookie_secret);
-        if(!token) {
+        const user = await User.findOne({ token: token });
+        if (!token) {
             console.log("Access Denied")
+            return res.status(403).send("Access Denied");
+        }else if(!user.userID){
+            console.log("Someone try to fuck up the AUTHENTICATE mechanism")
             return res.status(403).send("Access Denied");
         }
 
-        if(token.startsWith("Bearer ")) {
+        if (token.startsWith("Bearer ")) {
             token = token.slice(7, token.length).trimLeft();
         }
         console.log(token)
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         req.user = verified;
         next();
-    } catch(err) {
+    } catch (err) {
         console.log(err.message)
         res.status(500).json({ error: err.message });
     }
