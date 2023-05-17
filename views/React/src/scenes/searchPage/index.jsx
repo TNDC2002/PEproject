03aa68c64f0f908883d542f63ea600a9a26b34a0
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-05-18 00:14:15 +0700

    Finalize search UI

--- views/React/src/scenes/searchPage/index.jsx
+++ views/React/src/scenes/searchPage/index.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
-import { useTheme, Typography, Box, Button } from "@mui/material";
+import { useTheme, Typography, Box, Button, Grid } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import { useLocation } from "react-router-dom";
 import Navbar from "../navbar/index.jsx";
 import Loading from "../../components/Loading";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import Image from "mui-image";
+import FlexBetween from "../../components/FlexBetween.jsx";
 const SearchPage = () => {
   const [result, setResult] = useState(null);
   const dispatch = useDispatch();
@@ -32,12 +34,18 @@ const SearchPage = () => {
             (movie) => movie.media_type === "tv" || movie.media_type === "movie"
           )
           .map((movie) => ({
-            label: movie.original_name
+            title: movie.original_name
               ? movie.original_name
               : movie.original_title,
             id: movie.id,
-            poster_path: movie.backdrop_path,
+            poster_path: movie.poster_path,
             media_type: movie.media_type,
+            overview: movie.overview,
+            genre_ids: movie.genre_ids,
+            popularity: movie.popularity,
+            release_date: movie.release_date,
+            vote_average: movie.vote_average,
+            vote_count: movie.vote_count,
           }));
         setResult(results);
       } catch (err) {
@@ -60,29 +68,68 @@ const SearchPage = () => {
     );
   }
 
+  console.log(result);
   return (
     <div>
       <Navbar></Navbar>
-      {result.map((movie) => (
-        <div key={movie.id}>
-          <img
-            src={
-              movie.poster_path
-                ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-                : "https://via.placeholder.com/500x300.png?text=No+Image"
-            }
-            alt={movie.label}
-            onClick={() => {
-              if (movie.media_type === "movie") {
-                navigate(`/movie/${movie.id}`);
-              } else {
-                navigate(`/TV Shows/${movie.id}`);
-              }
-            }}
-          />
-          <p>{movie.label}</p>
-        </div>
-      ))}
+      <Box sx={{}}>
+        {result.map((movie) => (
+          <Grid container spacing={3} sx={{ my: 2 }}>
+            <Grid item key={movie.id} xs={12} sm={6} md={3} lg={3}>
+              <Box sx={{ position: "relative", display: "inline-flex" }}>
+                <Box
+                  sx={{
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.5)",
+                  }}
+                >
+                  <Image
+                    sx={{ borderRadius: "10px" }}
+                    src={
+                      movie.poster_path
+                        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                        : "https://via.placeholder.com/1000x1500.png?text=No Image Found"
+                    }
+                    alt={`${movie.title} poster`}
+                    onClick={() => {
+                      if (movie.media_type === "movie") {
+                        navigate(`/movie/${movie.id}`);
+                      } else {
+                        navigate(`/TV Shows/${movie.id}`);
+                      }
+                    }}
+                  />
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={9} lg={9}>
+              <Typography sx={{ fontSize: 40, fontWeight: "medium" }}>
+                {movie.title}
+              </Typography>
+
+              <Typography variant="body1" sx={{ my: 0.5 }}>
+                <strong>Overview:</strong> {movie.overview}
+              </Typography>
+
+              <Typography variant="body1" sx={{ my: 0.5 }}>
+                <strong>Popularity:</strong> {movie.popularity}
+              </Typography>
+
+              <Typography variant="body1" sx={{ my: 0.5 }}>
+                <strong>Release Date:</strong> {movie.release_date}
+              </Typography>
+
+              <Typography variant="body1" sx={{ my: 0.5 }}>
+                <strong>Vote Average:</strong> {movie.vote_average}
+              </Typography>
+
+              <Typography variant="body1" sx={{ my: 0.5 }}>
+                <strong>Vote Count:</strong> {movie.vote_count}
+              </Typography>
+            </Grid>
+          </Grid>
+        ))}
+      </Box>
     </div>
   );
 };
