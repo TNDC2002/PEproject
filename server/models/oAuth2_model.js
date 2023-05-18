8ee948725642238b161a8a83a4668a95bb9af56a
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 15:14:34 +0700

    Github oAuth2 model

--- server/models/oAuth2_model.js
+++ server/models/oAuth2_model.js
@@ -91,8 +91,53 @@ const Fb_Callback = async (req) => {
         }
     }
 }
+const Gh_Callback = async (req) => {
+    try {
+        const { GhId } = req.body;
+        const user = await User.findOne({ GgId: GhId });
+        if (!user) {
+            return {
+                status: 400,
+                error: "User does not exist."
+            }
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return {
+                status: 400,
+                error: "Invalid credentials."
+            }
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const salt = await bcrypt.genSalt();
+        const tokenHash = await bcrypt.hash(token, salt);
+        //save token to DB
+        let update = await User.findOneAndUpdate({ _id: user._id }, {
+            token: tokenHash
+        })
+            .then((update) => { })
+            .catch((error) => {
+                console.log("ERROR --- Auth.js --- can't UPDATE token to DB")
+            })
+
+        delete user.password;
+        return {
+         user: user,
+         token: token
+        }
+    } catch (err) {
+        console.log(err.message)
+        return{
+            status: 500,
+            error: err.message
+        }
+    }
+}
 let output = {
     Gg_Callback,
-    Fb_Callback
+    Fb_Callback,
+    Gh_Callback
 }
 export default output
