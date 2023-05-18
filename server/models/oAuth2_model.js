859675085117f722942af45664c0d87d0b971a98
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 16:11:08 +0700

    add twitter oAuth2 to model

--- server/models/oAuth2_model.js
+++ server/models/oAuth2_model.js
@@ -84,7 +84,46 @@ const Fb_Callback = async (req) => {
 const Gh_Callback = async (req) => {
     try {
         const { GhId } = req.body;
-        const user = await User.findOne({ GgId: GhId });
+        const user = await User.findOne({ GhId: GhId });
+        if (!user) {
+            return {
+                status: 400,
+                error: "User does not exist."
+            }
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const salt = await bcrypt.genSalt();
+        const tokenHash = await bcrypt.hash(token, salt);
+        //save token to DB
+        let update = await User.findOneAndUpdate({ _id: user._id }, {
+            token: tokenHash
+        })
+            .then((update) => { })
+            .catch((error) => {
+                return {
+                    status: 400,
+                    error: "fail to apply token"
+                }
+            })
+
+        delete user.password;
+        return {
+            user: user,
+            token: token
+        }
+    } catch (err) {
+        console.log(err.message)
+        return {
+            status: 500,
+            error: err.message
+        }
+    }
+}
+const Tw_Callback = async (req) => {
+    try {
+        const { TwId } = req.body;
+        const user = await User.findOne({ TwId: TwId });
         if (!user) {
             return {
                 status: 400,
