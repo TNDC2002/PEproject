75628712d01631ea99ba25d7862b7eb2584f4077
tqnaam <18770@student.vgu.edu.vn>
2023-05-19 00:06:08 +0700

    myList favourite movies UI done

--- views/React/src/scenes/mylistPage/index.jsx
+++ views/React/src/scenes/mylistPage/index.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Typography, Grid } from "@mui/material";
+import { Box, Typography, Grid, Rating } from "@mui/material";
 import Image from "mui-image";
 import Navbar from "../navbar";
+import { styled } from "@mui/system";
+import ReactPlayer from 'react-player/youtube';
+
 
 const MyListPage = () => {
     const [favouriteMovie, setFavouriteMovie ] = useState([]);
@@ -11,7 +14,7 @@ const MyListPage = () => {
     const [rentedShow, setRentedShow ] = useState([]);
     const user = useSelector((state) => state.user);
 
-    const[isHovered, setIsHovered] = useState(false)
+    const [hoveredItemId, setHoveredItemId] = useState(null);
 
     useEffect(() => {
         const fetchUserFavouriteMovieShow = async (userID) => {
@@ -33,78 +36,227 @@ const MyListPage = () => {
         }
         fetchUserFavouriteMovieShow(user._id);
     }, [])  
-    console.log(favouriteShow.length)
+    console.log(favouriteMovie)
 
-    const spinner = styled("Box")({
-        height: "50px",
-        width: "max-content",
+    const Spinner = styled("Box")({
+        height: "85vh",
+        width: "100vw",
         fontSize: "18px",
         fontWeight: 600,
-        spacing: "1rem",
+        letterSpacing: "1rem",
         color: "#f5f5f5",
-        filter: "drop-shadow(0 0 10px)",
+        filter: "drop-shadow(0 0 50px)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-      });
-
+        gap: '1rem',
+        "& .MuiTypography-root": {
+            fontSize: '30px',
+            animation: "bouncy 2.5s ease infinite",
+            filter: "drop-shadow(0 0 10px #f5f5f5)",
+            marginRight: "0.2em",
+            marginLeft: "0.2em",
+            "&:nth-child(1)": {
+              animationDelay: "0s",
+            },
+            "&:nth-child(2)": {
+              animationDelay: "0.25s",
+              padding:"0 5rem 0 0"
+            },
+            "&:nth-child(3)": {
+              animationDelay: "0.5s",
+            },
+            "&:nth-child(4)": {
+              animationDelay: "0.75s",
+            },
+            "&:nth-child(5)": {
+              animationDelay: "1s",
+            },
+            "&:nth-child(6)": {
+              animationDelay: "1.25s",
+            },
+            "&:nth-child(7)": {
+              animationDelay: "1.5s",
+            },
+            "&:nth-child(8)": {
+              animationDelay: "1.75s",
+            },
+            "&:nth-child(9)": {
+              animationDelay: "2s",
+            },
+            "&:nth-child(10)": {
+              animationDelay: "2.25s",
+            },
+            "&:nth-child(11)": {
+              animationDelay: "2.5s",
+            },
+            "&:nth-child(12)": {
+              animationDelay: "2.75s",
+            },
+            "&:nth-child(13)": {
+              animationDelay: "3s",
+            },
+            "&:nth-child(14)": {
+              animationDelay: "3.25s",
+            },
+          },
+        "@keyframes bouncy": {
+            "0%, 100%": {
+            transform: "translateY(0)",
+            },
+            "50%": {
+            transform: "translateY(-50px)",
+            },
+        },
+    });
+    
     return (
         <Box>
             <Navbar></Navbar>
-            { favouriteMovie.length === 0 ? 
-                <Typography>LIKE SOMETHING BITCH</Typography>
-             : 
-                <Box sx={{
-                    margin: '1.5rem'
-                }}>
-                    <Box>
-                        <Typography sx={{
-                            fontSize:'1.5rem',
-                            fontWeight:'bold',
-                            borderBottom: '2px solid white',
-                            paddingBottom: '0.5rem'
-                        }}>Your favourite movies</Typography>
-                        <Box sx={{ padding:'1rem 0', display: 'flex', flexWrap: 'wrap', gap: '0.75rem' }}>
-                            {favouriteMovie.map((movie) => (
-                            <Grid title={movie.title} key={movie.id} item xs={12} sm={6} md={4} lg={3}>
-                                <Box sx={{
-                                    display: 'inline-block',
-                                    "&:hover":{
-                                        cursor: 'pointer',
-                                        boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.5)",
-                                    }
-                                }}>
-                                    <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
-                                </Box>
-                            </Grid>
-                            ))}
+            <Box>
+                { favouriteMovie.length === 0 && favouriteShow.length === 0 ? 
+                    <Spinner>
+                        <Typography>N</Typography>
+                        <Typography>O</Typography>
+                        <Typography>I</Typography>
+                        <Typography>N</Typography>
+                        <Typography>F</Typography>
+                        <Typography>O</Typography>
+                        <Typography>R</Typography>
+                        <Typography>M</Typography>
+                        <Typography>A</Typography>
+                        <Typography>T</Typography>
+                        <Typography>I</Typography>
+                        <Typography>O</Typography>
+                        <Typography>N</Typography>
+                        {/* <ReactPlayer
+                            url={`https://www.youtube.com/watch?v=6Cr_8tvvQ0k`}
+                            controls={false}
+                            playing={true}
+                            loop={true}
+                            volume={1}
+                            width="500px"
+                            height="500px"
+                        /> */}
+                    </Spinner>
+                : 
+                    <Box
+                    sx={{
+                        margin: '1.5rem'
+                    }}>
+                        <Box>
+                            <Typography sx={{
+                                fontSize:'1.5rem',
+                                fontWeight:'bold',
+                                borderBottom: '2px solid white',
+                                paddingBottom: '0.5rem'
+                            }}>Your favourite movies</Typography>
+                            <Box 
+                            className="moviePoster" 
+                            sx={{ 
+                                padding:'1rem 0', 
+                                display: 'flex', 
+                                flexWrap: 'wrap', 
+                                gap: '1rem' 
+                            }}>
+                                {favouriteMovie.map((movie) => (
+                                <Grid title={movie.title} key={movie.id} item xs={12} sm={6} md={4} lg={3}>
+                                    <Box
+                                    onMouseEnter={() => setHoveredItemId(movie.id)}
+                                    onMouseLeave={() => setHoveredItemId(null)}
+                                    sx={{
+                                        position: 'relative',
+                                        display: 'flex',
+                                        "&:hover":{
+                                            cursor: 'pointer',
+                                            boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.5)",
+                                        }
+                                    }}>
+                                        <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+                                        {/* <Typography>{movie.title}</Typography> */}
+                                        {hoveredItemId === movie.id && (
+                                            <Box className="hover" sx={{
+                                                display:'flex',
+                                                position: 'absolute',
+                                                width: '100%',
+                                                height: '100%',
+                                                opacity: 0.8
+                                            }}>
+                                                <Box className="backdropContainer" sx={{
+                                                    backgroundColor: 'black',
+                                                    opacity: 1,
+                                                    width: '100%',
+                                                    height: '100%',
+                                                    alignItems: 'center',
+                                                    justifyContent: 'center',
+
+                                                }}>
+                                                    <Typography sx={{
+                                                        fontSize: '1.25rem',
+                                                        fontWeight: 'bold',
+                                                        
+                                                    }}>{movie.title}</Typography>
+                                                    <Rating name="read-only" value={movie.vote_average /2} precision="0.5" readOnly></Rating> 
+                                                    <Typography>IMDB: {movie.vote_average} / 10</Typography>
+                                                    
+                                                    {/*      TIME-RENTED ( CALCULATE TO THE TIME EXPIRED )        */}
+                                                    <Typography></Typography>
+                                                    {/* TAKING IMDB POINT NOT RATING FROM USER */}
+                                                    {/* <ReactPlayer
+                                                        url={`https://www.youtube.com/watch?v=6Cr_8tvvQ0k`}
+                                                        controls={false}
+                                                        playing={true}
+                                                        loop={true}
+                                                        volume={1}
+                                                        width="100%"
+                                                        height="100%"
+                                                    /> */}
+                                                </Box>
+                                                <Box className="infoContainer">
+                                                    {/* <Typography className="Title" sx={{
+                                                        color: 'black'
+                                                    }}>
+                                                        {movie.title}
+                                                    </Typography> */}
+                                                    {/* <Box className="icons">
+                                                        <Box className="controls">
+
+                                                        </Box>
+                                                    </Box> */}
+                                                </Box>
+                                            </Box>
+                                        )}
+                                    </Box>
+                                </Grid>
+                                ))}
+                            </Box>
+                        </Box>
+                        <Box>
+                            <Typography sx={{
+                                fontSize:'1.5rem',
+                                fontWeight:'bold',
+                                borderBottom: '2px solid white',
+                                paddingBottom: '0.5rem'
+                            }}>Your favourite shows</Typography>
+                            <Box sx={{ padding:'1rem 0', display: 'flex', flexWrap: 'wrap', gap: '0.75rem' }}>
+                                {favouriteShow.map((show) => (
+                                <Grid title={show.name} key={show.id} item lg={3}>
+                                    <Box sx={{
+                                        display: 'inline-block',
+                                        "&:hover":{
+                                            cursor: 'pointer',
+                                            boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.5)",
+                                        }
+                                    }}>
+                                        <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${show.seasons[show.intendedSeason].poster_path}`}/>
+                                    </Box>
+                                </Grid>
+                                ))}
+                            </Box>
                         </Box>
                     </Box>
-                    <Box>
-                        <Typography sx={{
-                            fontSize:'1.5rem',
-                            fontWeight:'bold',
-                            borderBottom: '2px solid white',
-                            paddingBottom: '0.5rem'
-                        }}>Your favourite shows</Typography>
-                        <Box sx={{ padding:'1rem 0', display: 'flex', flexWrap: 'wrap', gap: '0.75rem' }}>
-                            {favouriteShow.map((show) => (
-                            <Grid title={show.name} key={show.id} item lg={3}>
-                                <Box sx={{
-                                    display: 'inline-block',
-                                    "&:hover":{
-                                        cursor: 'pointer',
-                                        boxShadow: "0px 0px 30px rgba(255, 255, 255, 0.5)",
-                                    }
-                                }}>
-                                    <Image width="175px" height="275px" src={`https://image.tmdb.org/t/p/w500${show.seasons[show.intendedSeason].poster_path}`}/>
-                                </Box>
-                            </Grid>
-                            ))}
-                        </Box>
-                    </Box>
-                </Box>
-            }
+                }
+            </Box>
         </Box>
     )
 }
