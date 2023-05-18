066346ebd9247fc7ee4b649fa16ff51c8db70c35
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 14:47:28 +0700

    move passport setup to webroutes.js

--- server/routes/WebRoutes.js
+++ server/routes/WebRoutes.js
@@ -4,8 +4,67 @@
 
 import express from "express";
 import * as uploader from "../middleware/FileUploader.js"
+import passport from 'passport'
+import { OAuth2Strategy as GoogleStrategy } from "passport-google-oauth";
+import { Strategy as FacebookStrategy } from "passport-facebook";
+
 //uploader setup
 var upload = uploader.default()
+//Passport setup
+passport.use(
+    "google",
+    new GoogleStrategy(
+        {
+            clientID: process.env.GG_Client_ID,
+            clientSecret: process.env.GG_Client_secret,
+            callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            const GgId = profile.id;
+            const firstName = profile.name.givenName;
+            const lastName = profile.name.familyName;
+            const email = profile.emails[0].value;
+            const picturePath = profile.photos[0].value;
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                picturePath,
+                GgId
+            })
+            const savedUser = newUser.save();
+            done(null, { id: GgId });
+
+        }
+    )
+);
+passport.use(
+    "facebook",
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_APP_ID,
+            clientSecret: process.env.FACEBOOK_APP_SECRET,
+            callbackURL: "/auth/facebook/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            const FbId = profile.id;
+            const firstName = profile.name.givenName;
+            const lastName = profile.name.familyName;
+            const email = profile.emails[0].value;
+            const picturePath = profile.photos[0].value;
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                picturePath,
+                FbId
+            })
+            const savedUser = newUser.save();
+            done(null, { id: FbId });
+
+        }
+    )
+);
 
 /* Import your controller here by syntax:
     import * as <your controller name> from "../controller/<ControllerFile>.js" */
