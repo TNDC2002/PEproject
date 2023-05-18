baa73143382f4a94b1e01677c73d043338b3a79f
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 15:20:49 +0700

    add Github oAuth to controller

--- server/controller/oAuth2_Controller.js
+++ server/controller/oAuth2_Controller.js
@@ -29,8 +29,19 @@ const FB_oAuth2 = async (req, res) => {
     }
 }
 
+const GH_oAuth2 = async (req, res) => {
+    let oAuth2_return = await oAuth2.Gh_Callback(req)
+    if (oAuth2_return.status) {
+        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
+    }else{
+        res.cookie('token', token, cookieOptions);
+        res.status(200).json({ user: oAuth2_return.user });
+    }
+}
+
 const output = {
     GG_oAuth2,
-    FB_oAuth2
+    FB_oAuth2,
+    GH_oAuth2
 }
 export default output
