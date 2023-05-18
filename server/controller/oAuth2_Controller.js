58449ff65a4aab8fb72717727006b983bcd4f654
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 14:13:59 +0700

    import model function to controller

--- server/controller/oAuth2_Controller.js
+++ server/controller/oAuth2_Controller.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import oAuth2 from '../models/oAuth2_model.js'
 import * as dotenv from 'dotenv';
 dotenv.config()
 
@@ -8,10 +9,10 @@ dotenv.config()
 app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 
 // Google authentication callback route
-app.get("/auth/google/callback", passport.authenticate("google", { successRedirect: "/", failureRedirect: "/login" }));
+app.get("/auth/google/callback", passport.authenticate("google", { successRedirect: "/", failureRedirect: "/login" }),oAuth2.Gg_Callback);
 
 // Facebook authentication route
 app.get("/auth/facebook", passport.authenticate("facebook"));
 
 // Facebook authentication callback route
-app.get("/auth/facebook/callback", passport.authenticate("facebook", { successRedirect: "/", failureRedirect: "/login" }));
+app.get("/auth/facebook/callback", passport.authenticate("facebook", { successRedirect: "/", failureRedirect: "/login" }),oAuth2.Fb_Callback);
--- server/models/oAuth2_model.js
+++ server/models/oAuth2_model.js
@@ -57,3 +57,80 @@ passport.use(
         }
     )
 );
+
+const Gg_Callback = async (req)=>{
+    try {
+        const { GgId } = req.body;
+        const user = await User.findOne({ GgId: GgId });
+        if (!user) return res.status(400).json({ msg: "User does not exist." })
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." })
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const salt = await bcrypt.genSalt();
+        const tokenHash = await bcrypt.hash(token, salt);
+        //save token to DB
+        let update = await User.findOneAndUpdate({ _id: user._id }, {
+            token: tokenHash
+        })
+            .then((update) => { })
+            .catch((error) => {
+                console.log("ERROR --- Auth.js --- can't UPDATE token to DB")
+            })
+
+        delete user.password;
+        const cookieOptions = {
+            maxAge: 36000000, // Cookie expiration time (in milliseconds)
+            httpOnly: true, // Restrict cookie access to HTTP requests only
+            signed: true, // Enable cookie signing
+            sameSite: 'Lax'
+        };
+        res.cookie('token', token, cookieOptions);
+        res.status(200).json({ user });
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const Fb_Callback = async (req)=>{
+    try {
+        const { FbId } = req.body;
+        const user = await User.findOne({ FbId: FbId });
+        if (!user) return res.status(400).json({ msg: "User does not exist." })
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." })
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        const salt = await bcrypt.genSalt();
+        const tokenHash = await bcrypt.hash(token, salt);
+        //save token to DB
+        let update = await User.findOneAndUpdate({ _id: user._id }, {
+            token: tokenHash
+        })
+            .then((update) => { })
+            .catch((error) => {
+                console.log("ERROR --- Auth.js --- can't UPDATE token to DB")
+            })
+
+        delete user.password;
+        const cookieOptions = {
+            maxAge: 36000000, // Cookie expiration time (in milliseconds)
+            httpOnly: true, // Restrict cookie access to HTTP requests only
+            signed: true, // Enable cookie signing
+            sameSite: 'Lax'
+        };
+        res.cookie('token', token, cookieOptions);
+        res.status(200).json({ user });
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).json({ error: err.message });
+    }
+}
+let output = {
+    Gg_Callback,
+    Fb_Callback
+}
+export default output
