3b9ec293fd0a8e1a521463c3e1b82efb934eeb06
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-18 23:24:31 +0700

    apply redirect method

--- server/controller/oAuth2_Controller.js
+++ server/controller/oAuth2_Controller.js
@@ -12,39 +12,63 @@ const cookieOptions = {
 const GG_oAuth2 = async (req, res) => {
     let oAuth2_return = await oAuth2.Gg_Callback(req)
     if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+        console.log("status: ",oAuth2_return.status, "error:",oAuth2_return.error );
+    } else {
+        const serializedCookieOptions = JSON.stringify(cookieOptions);
+        const queryParams = querystring.stringify({
+            token: token,
+            user: oAuth2_return.user,
+            cookieOptions: serializedCookieOptions,
+        });
+        const redirectUrl = `http://localhost:5173/auth/callback?${queryParams}`;
+        res.redirect(redirectUrl);
     }
 }
 
 const FB_oAuth2 = async (req, res) => {
     let oAuth2_return = await oAuth2.Fb_Callback(req)
     if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+        console.log("status: ",oAuth2_return.status, "error:",oAuth2_return.error );
+    } else {
+        const serializedCookieOptions = JSON.stringify(cookieOptions);
+        const queryParams = querystring.stringify({
+            token: token,
+            user: oAuth2_return.user,
+            cookieOptions: serializedCookieOptions,
+        });
+        const redirectUrl = `http://localhost:5173/auth/callback?${queryParams}`;
+        res.redirect(redirectUrl);
     }
 }
 
 const GH_oAuth2 = async (req, res) => {
     let oAuth2_return = await oAuth2.Gh_Callback(req)
     if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+        console.log("status: ",oAuth2_return.status, "error:",oAuth2_return.error );
+    } else {
+        const serializedCookieOptions = JSON.stringify(cookieOptions);
+        const queryParams = querystring.stringify({
+            token: token,
+            user: oAuth2_return.user,
+            cookieOptions: serializedCookieOptions,
+        });
+        const redirectUrl = `http://localhost:5173/auth/callback?${queryParams}`;
+        res.redirect(redirectUrl);
     }
 }
 const TW_oAuth2 = async (req, res) => {
     let oAuth2_return = await oAuth2.Tw_Callback(req)
     if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+        console.log("status: ",oAuth2_return.status, "error:",oAuth2_return.error );
+    } else {
+        const serializedCookieOptions = JSON.stringify(cookieOptions);
+        const queryParams = querystring.stringify({
+            token: token,
+            user: oAuth2_return.user,
+            cookieOptions: serializedCookieOptions,
+        });
+        const redirectUrl = `http://localhost:5173/auth/callback?${queryParams}`;
+        res.redirect(redirectUrl);
     }
 }
 
