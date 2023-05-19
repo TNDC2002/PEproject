b6031f387a08561e9b7918bbd701deca4f8855f0
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-19 22:04:04 +0700

    add bug detect functionlity to controller

--- server/controller/oAuth2_Controller.js
+++ server/controller/oAuth2_Controller.js
@@ -10,41 +10,59 @@ const cookieOptions = {
 };
 
 const GG_oAuth2 = async (req, res) => {
-    let oAuth2_return = await oAuth2.Gg_Callback(req)
-    if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', oAuth2_return.token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+    try {
+        let oAuth2_return = await oAuth2.Gg_Callback(req)
+        if (oAuth2_return.status) {
+            return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
+        } else {
+            res.cookie('token', oAuth2_return.token, cookieOptions);
+            res.status(200).json({ user: oAuth2_return.user });
+        }
+    } catch (err) {
+        console.log("/login/google --- error:", err.message)
     }
+
 }
 
 const FB_oAuth2 = async (req, res) => {
-    let oAuth2_return = await oAuth2.Fb_Callback(req)
-    if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', oAuth2_return.token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+    try {
+        let oAuth2_return = await oAuth2.Fb_Callback(req)
+        if (oAuth2_return.status) {
+            return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
+        } else {
+            res.cookie('token', oAuth2_return.token, cookieOptions);
+            res.status(200).json({ user: oAuth2_return.user });
+        }
+    } catch (err) {
+        console.log("/login/facebook --- error:", err.message)
     }
 }
 
 const GH_oAuth2 = async (req, res) => {
-    let oAuth2_return = await oAuth2.Gh_Callback(req)
-    if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', oAuth2_return.token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+    try {
+        let oAuth2_return = await oAuth2.Gh_Callback(req)
+        if (oAuth2_return.status) {
+            return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
+        } else {
+            res.cookie('token', oAuth2_return.token, cookieOptions);
+            res.status(200).json({ user: oAuth2_return.user });
+        }
+    } catch (err) {
+        console.log("/login/github --- error:", err.message)
     }
+
 }
 const TW_oAuth2 = async (req, res) => {
-    let oAuth2_return = await oAuth2.Tw_Callback(req)
-    if (oAuth2_return.status) {
-        return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
-    }else{
-        res.cookie('token', oAuth2_return.token, cookieOptions);
-        res.status(200).json({ user: oAuth2_return.user });
+    try {
+        let oAuth2_return = await oAuth2.Tw_Callback(req)
+        if (oAuth2_return.status) {
+            return res.status(oAuth2_return.status).json({ error: oAuth2_return.error });
+        } else {
+            res.cookie('token', oAuth2_return.token, cookieOptions);
+            res.status(200).json({ user: oAuth2_return.user });
+        }
+    } catch (err) {
+        console.log("/login/twitter --- error:", err.message)
     }
 }
 
