1acaa833ecaffb24be422ddce60c7c4e99d9726a
tqnaam <18770@student.vgu.edu.vn>
2023-05-21 01:14:20 +0700

    Create Admin_Schema.js

---
+++ server/models/Admin_Schema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema(
+  {
+    movieID: {
+      type: String,
+      required: true,
+    },
+    media_type: {
+      type: String,
+      required: true,
+    },
+    season: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    isAvailable: {
+      type: Boolean,
+      required: true,
+      default: 1,
+    },
+  },
+  { timestamps: true }
+);
+
+const Admin = mongoose.model("Admin", UserSchema);
+export default Admin;
