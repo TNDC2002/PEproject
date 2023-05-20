c0821130ad7c63d600c4633b4fcd485f4b57faf7
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-05-21 00:35:57 +0700

    setup model

---
+++ server/models/AdminChecker_model.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User_Schema.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
+const Admin_checker = async (req, res) => {
+    try {
+        let token = req.signedCookies.token;
+        if (!token) {
+            return {
+                status: 200,
+                authenticated: false
+            }
+        }
+        const UUID = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: UUID.id });
+        const isMatch = await bcrypt.compare(token, user.token);
+        if (isMatch && user.isAdmin == true) {
+            console.log("___res true")
+            return {
+                status: 200,
+                isAdmin: isAdmin
+            }
+        } else {
+            return {
+                status: 403,
+                err: "access denied!"
+            }
+        }
+
+    } catch (err) {
+        console.log(err.message)
+        return {
+            status: 500,
+            err: err.message
+        }
+    }
+}
+const output = {
+    Admin_checker
+}
+export default output;
