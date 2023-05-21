1bbaf71dea4e24388980ece83eb27cb2f921e419
Khang <khanglun20000@gmail.com>
2023-05-21 17:09:19 +0700

    Update vite.config.js

--- views/React/vite.config.js
+++ views/React/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "https://vgu-tinyprojects-pe2023-vgupe2023-team5-w71a.vercel.app/"
+      "/api": "http://localhost:5000/"
     }
   },
   plugins: [react()],
