0cf06438f1f2555af602a8f9b57ae9d9a624726d
Ha Quach Phu Thanh <91040962+MapleThanh@users.noreply.github.com>
2023-05-24 16:42:54 +0700

    add with include credential  to true

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ const allowedOrigins = [
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", `${process.env.FRONTEND_URL}`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials (cookies) in cross-origin requests
   next();
 });
 
